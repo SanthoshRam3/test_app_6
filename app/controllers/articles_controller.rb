43f204eb93508ec3caee8ae981560a8f21a7fcb5
Santhosh Ram <santhoshram03111998@gmail.com>
2023-07-18 10:54:59 +0530

    Modified code in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ end
 def new
 end
 def create
-    
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save
+    #redirect_to articles_path(@article)
+    redirect_to @article
 end
 end
