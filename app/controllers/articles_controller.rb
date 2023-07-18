cd46beb8d9a0db3b589039872f757d7cd7570600
Santhosh Ram <santhoshram03111998@gmail.com>
2023-07-18 13:27:48 +0530

    Adding edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,31 @@ def index
     @articles = Article.all
 end
 def new
+    @article = Article.new
+end
+def edit
+    @article = Article.find(params[:id])
 end
 def create
     @article = Article.new(params.require(:article).permit(:title,:description))
-    @article.save
+    if @article.save 
     #redirect_to articles_path(@article)
+    flash[:notice] = "Articles Was Succesfully Saved"
     redirect_to @article
+
+    else 
+        render 'new'
+    end
+end
+
+def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article
+    else
+        render 'edit'
+    end
+
 end
 end
